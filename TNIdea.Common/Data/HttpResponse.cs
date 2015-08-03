2afa8f790e175cdf29d2f4cfdcfb5aa29297d038
Carey Tzou <ziyunhx@gmail.com>
2015-08-03 23:46:44 +0800

    Replace TNIdea.Common to libs.

--- TNIdea.Common/Data/HttpResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TNIdea.Common.Data
-{
-    public class HttpResponse
-    {
-        public int HttpCode { set; get; }
-        public string Content { set; get; }
-        public string Url { set; get; }
-    }
-}
