8baee79ff4c838a3782806dcce1c5c9f2355a9f2
Carey Tzou <ziyunhx@gmail.com>
2015-08-09 23:56:17 +0800

    add ImitateLogin.Service project.

---
+++ ImitateLogin.Service/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thrift.Server;
+using Thrift.Transport;
+
+namespace ImitateLogin.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int port = 7901;
+
+            string str = ConfigurationManager.AppSettings["ServerPort"];
+            if (!string.IsNullOrEmpty(str))
+                int.TryParse(str, out port);
+
+            if (args != null && args.Length == 1)
+            {
+                int.TryParse(args[0], out port);
+            }
+
+            Start(port);
+        }
+
+        public static void Start(int port)
+        {
+            TServerSocket serverTransport = new TServerSocket(port, 0, false);
+            Login.Processor processor = new Login.Processor(new LoginHelper());
+            TServer server = new TSimpleServer(processor, serverTransport);
+            Console.WriteLine("Starting server on port {0} ...", port);
+            server.Serve();
+        }
+    }
+}
