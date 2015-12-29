9139ac20942cdaf87208ffd63ddd2babb4e277fb
Carey Tzou <ziyunhx@gmail.com>
2015-12-29 22:24:48 +0800

    add mail notication.

--- Extensions/MailNotication/Controllers/MailController.cs
+++ Extensions/MailNotication/Controllers/MailController.cs
@@ -7,19 +7,16 @@ namespace MailNotication.Controllers
     public class MailController : ApiController
     {
         /// <summary>
-        /// 
+        /// Send 
         /// </summary>
         /// <returns></returns>
         [HttpGet]
         public string SendMail(LoginSite loginSite, string imageUrl)
         {
-            return "success";
-        }
-
-        [HttpPost]
-        public string SendMail(OperationObj operationObj)
-        {
-            return "success";
+            if (MailHelper.SendEmail(loginSite.ToString() + "登录中，请求协助！", string.Format("<img src=\"{0}\"><br /> {0}", imageUrl)))
+                return "success";
+            else
+                return "";
         }
     }
 }
